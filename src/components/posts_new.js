8b077c97edf90be4cf678e23413000e00fe23720
PRANAVBHALE <bhalepranav25@gmail.com>
2017-12-23 19:40:02 +0530

    Routing Posts/New

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,19 @@
 import React,{Component} from 'react'
 import {Field,reduxForm} from 'redux-form'
+import {Link} from 'react-router-dom'
+
 
 class PostsNew extends Component{
 
   renderField(field){
   //  debugger
+
+  const {meta:{touched,error}}=field
+  const className=`form-group ${touched && error ? 'has-danger' : ''}`
+
+
     return(
-      <div className="form-group">
+      <div className={className}>
       <label>{field.label}</label>
         <input
         className="form-control"
@@ -14,17 +21,26 @@ class PostsNew extends Component{
         {...field.input}
 
         />
-        {field.meta.error}
+        <div className="text-help">
+        {touched ? error : ''}
+
+        </div>
       </div>
     )
   }
 
 
-
+onSubmit(values){
+  console.log(values);
+}
 
   render(){
+
+    const {handleSubmit} = this.props
+
+
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
         label="Title"
         name="title"
@@ -40,6 +56,10 @@ class PostsNew extends Component{
         name="content"
         component={this.renderField}
         />
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger" >Cancel </Link>
+
       </form>
     )
   }
